b237c28650aaffa56e3b7e25df92f7b85ef9f1ab
Alex Gladilov <a.gladilov@yandex.ru>
2015-11-05 23:47:03 +0300

    Change ajax request

--- js/script.js
+++ js/script.js
@@ -17,7 +17,8 @@
 
   $(document).on( "pagecontainershow", function( event, ui ) {
     var activePage = ui.toPage[0].id;
-    //if (activePage == 'page-news') {
+    
+    if (activePage == 'page-news') {
       
       $.ajax({
         //url: "http://dev.opennov.ru/rest/views/news?display_id=rest",
@@ -25,7 +26,6 @@
         type: 'get',
         dataType: 'json',
         timeout: 3000,
-        //jsonp: 'jsoncallback',
         error: function (XMLHttpRequest, textStatus, errorThrown) {
           //console.log(JSON.stringify(XMLHttpRequest));
           //console.log(JSON.stringify(textStatus));
@@ -52,12 +52,81 @@
           $("#news-list").listview("destroy").listview();*/
         }
       });
-    //}
+    }
+    
+    else if (activePage == 'page-evets') {
+      
+      $.ajax({
+        //url: "http://dev.opennov.ru/rest/views/news?display_id=rest",
+        url: "http://dev.opennov.ru/rest/node.json",
+        type: 'get',
+        dataType: 'json',
+        timeout: 3000,
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+          //console.log(JSON.stringify(XMLHttpRequest));
+          //console.log(JSON.stringify(textStatus));
+          //console.log(JSON.stringify(errorThrown));
+          alert('error');
+          alert(textStatus);
+          alert(errorThrown);
+          
+          /*navigator.notification.alert(
+            'Ошибка ajax запроса..',
+            null,
+            'Ошибка',
+            'Ок'
+          );*/
+        },
+        success: function (data) {
+          alert('ok');
+          console.log(data);
+          
+          /*$("#news-list").html("");
+          $.each(data,function (i,node) {
+            $("#news-list").append($("<li></li>",{"html":node.node_title}));
+          });
+          $("#news-list").listview("destroy").listview();*/
+        }
+      });
+    }
+    
+    else if (activePage == 'page-blogs') {
+      
+      $.ajax({
+        //url: "http://dev.opennov.ru/rest/views/news?display_id=rest",
+        url: "http://dev.opennov.ru/rest/node/300.json",
+        type: 'get',
+        dataType: 'json',
+        timeout: 3000,
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+          //console.log(JSON.stringify(XMLHttpRequest));
+          //console.log(JSON.stringify(textStatus));
+          //console.log(JSON.stringify(errorThrown));
+          alert('error');
+          alert(textStatus);
+          alert(errorThrown);
+          
+          /*navigator.notification.alert(
+            'Ошибка ajax запроса..',
+            null,
+            'Ошибка',
+            'Ок'
+          );*/
+        },
+        success: function (data) {
+          alert('ok');
+          console.log(data);
+          
+          /*$("#news-list").html("");
+          $.each(data,function (i,node) {
+            $("#news-list").append($("<li></li>",{"html":node.node_title}));
+          });
+          $("#news-list").listview("destroy").listview();*/
+        }
+      });
+    }
+    
+    
   });
   
 })(jQuery);
-
-/*document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady() {
-  alert('deviceready');
-}*/
